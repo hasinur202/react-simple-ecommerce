79c2d083f8d7453ea72774a4edeceb7fb50dd279
Hasinur Rahman <hasinur202@gmail.com>
2021-10-17 00:07:40 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// console.log(process.env);
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+// };
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCq5x1zGN9yfs8x2twW5Cezu63RPINyLMA",
+    authDomain: "ema-john-simple-a0197.firebaseapp.com",
+    projectId: "ema-john-simple-a0197",
+    storageBucket: "ema-john-simple-a0197.appspot.com",
+    messagingSenderId: "943245832565",
+    appId: "1:943245832565:web:6ca119ef55fb5262eee33c"
+  };
+
+export default firebaseConfig;
